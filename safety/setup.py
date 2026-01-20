cea1c585f7bcc08b7a4353ba91f92faacf55ae6f
aso04 <augustin.so@gmail.com>
2026-01-20 15:41:23 -0800

    init safety node, added gitignore

--- safety/setup.py
+++ safety/setup.py
@@ -13,14 +13,13 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='zhou',
-    maintainer_email='guanxu@student.ubc.ca',
-    description='The safety node to prevent from crashing',
+    maintainer='poggers',
+    maintainer_email='poggers@todo.todo',
+    description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "safety_node = safety.safety_node:main"
         ],
     },
 )
