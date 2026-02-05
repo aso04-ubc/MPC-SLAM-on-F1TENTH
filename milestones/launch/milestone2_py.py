bcb36f1cd69ec0f6188fdd5bbd505c4ca3f73e9a
aso04 <augustin.so@gmail.com>
2026-02-05 14:50:55 -0800

    minor fixes

---
+++ milestones/launch/milestone2_py.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='safety',
+            executable='safety_node',
+            name='safety_node_launch',
+            output='screen',
+            emulate_tty=True,
+        ),
+
+        # Node(
+        #     package='wall_follow',
+        #     executable='wall_follow_node',
+        #     name='wall_follow_node_launch',
+        #     output='screen',
+        # ),
+
+        Node(
+            package='gap_following',
+            executable='gap_following',
+            name='gap_following_launch',
+            output='screen',
+        ),
+    ])
